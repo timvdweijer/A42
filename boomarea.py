5a99417bfaec6c8546e0910f020821a0377d63c6
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-18 17:31:04 +0100

    update

--- boomarea.py
+++ boomarea.py
@@ -1,10 +1,15 @@
 import numpy as np
-<<<<<<< HEAD
 from constants import *
-=======
-#import reactionforces
->>>>>>> 6f98c76c556d6f0426b8107d2a64ed5cb695f318
 
 
-coordinates = np.zeros((2,2))
+"""
+calculate location stringers with origin at hinge location
+"""
+coord_stringers = np.zeros((13,13))
+l_straight = np.sqrt((C_a - h/2.)**2 + (.5*h)**2)
+circumference = 2*l_straight + np.pi*h/2.
+d_st = circumference/(n_st + 1) #discussion with simon  if using 13 or 13+1
+for i in range(n_st):
+    di
+
 
