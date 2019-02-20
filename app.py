e80e9860a2593a15607fb20b2b37ab6ebe6b500d
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-20 22:13:15 +0100

    app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-import numpy as np
-#import constants
+import constants
+import coordinates
+import math
 
-stringercoordsz = [-1.,2.,2.,-1.]
-stringercoordsy = [2.,1.,-1.,-2.]
+a = coordinate(constants.C_a, constants.h/2, constants.n_st)
+
+stringercoordsy = a[0]
+stringercoordsz = a[1]
 
 class stringer:
 
@@ -15,19 +18,26 @@ stringers = []
 for i in range(len(stringercoordsz)):
     stringers.append(stringer(stringercoordsz[i],stringercoordsy[i]))
 
-A_st = 5
-n_st = 4
+A_st = constants.w_st * constants.t_st + constants.h_st * constants.t_st
+n_st = 15
 num_lst = []
 den_lst = []
 
-for i in range(n_st):
+for i in range(n_st):                                   #centroid of aileron
     num_lst.append(stringers[i].zcoord * A_st)
     den_lst.append(A_st)
+
+num_lst.append(2*(constants.h/math.pi)*constants.t_sk*math.pi*0.5*constants.h*0.5)  #contribution of 2 quarter circles
+den_lst.append(math.pi*constants.h*0.5*constants.t_sk)
+
+num_lst.append(2*constants.t_sk*math.sqrt((constants.h*0.5)**2+(constants.C_a-constants.h*0.5)**2)*
+               math.sqrt((constants.h*0.5)**2+(constants.C_a-constants.h*0.5)**2)*0.5*(-1)*math.cos(math.atan2(constants.h*0.5,constants.C_a-constants.h*0.5))) #contribution of straight parts
+den_lst.append(constants.t_sk*math.sqrt((constants.h*0.5)**2+(constants.C_a-constants.h*0.5)**2))
 
 centroid_y = 0
 centroid_z = sum(num_lst)/sum(den_lst)
 
-#Moments of Inertia about Centroid
+#Moments of Inertia ABOUT CENTROID
 
 I_yy_lst = []
 for i in range(n_st):
@@ -38,6 +48,8 @@ I_zz_lst = []
 for i in range(n_st):
     I_zz_lst.append(A_st*(stringers[i].ycoord-centroid_y)**2)
 I_zz = sum(I_zz_lst)
+
+#### ADD terms for semicircle, spar and thin plates
 
 print(I_yy)
 print(I_zz)
