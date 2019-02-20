a7e5d8133134b79e95fa1fa4392d35ea5b6412d6
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-20 13:31:27 +0100

    almost coordinates finished

--- boomarea.py
+++ boomarea.py
@@ -15,8 +15,8 @@ alpha = math.atan2(constants.h/2., constants.C_a - constants.h / 2.  )          
 
 
 
-coordinatesz = [0] * int(constants.n_st )
-coordinatesy = [0] * int(constants.n_st )
+coordinatesz = [0] * int(constants.n_st +2)
+coordinatesy = [0] * int(constants.n_st +2)
 
 for i in range(1, constants.n_st + 1):#""""check of deze plus 1 klopt"""
     if (d_st* i-  d_st/2.)< l_straight:
@@ -24,25 +24,50 @@ for i in range(1, constants.n_st + 1):#""""check of deze plus 1 klopt"""
         coordinatesy[i - 1] = - math.sin(alpha)* (d_st* i-  d_st/2.)
         end_straight_length = d_st* i-  d_st/2.
         j = copy.deepcopy(i)  
-        print (l_straight - (d_st * i -  d_st/2.))
     elif (d_st* i-  d_st/2.) < (l_semi_circle + l_straight):
-        print ("i am here")
         beta = (d_st*i - d_st* j - (l_straight - end_straight_length)) / (constants.h/2) 
-        coordinatesz[i - 1] = math.sin((beta)) * constants.h/2. 
-        coordinatesy[i - 1] = - math.cos((beta)) * constants.h/2.
+        coordinatesz[i ] = math.sin((beta)) * constants.h/2. 
+        coordinatesy[i ] = - math.cos((beta)) * constants.h/2.
         b = copy.deepcopy(i)
     elif (d_st* i-  d_st/2.) < (l_semi_circle + l_straight* 2.):
-        print (i, (((d_st * i - d_st/2) - (l_semi_circle + l_straight))))
-# =============================================================================
-#         coordinatesz[i - 1] =  math.cos(alpha)* (l_straight - ((d_st * i - d_st/2) - (l_semi_circle + l_straight)))  - (constants.C_a - constants.h/2)
-#         coordinatesy[i - 1] = - math.sin(alpha)* (l_straight - (d_st * i - d_st/2 - (l_semi_circle + l_straight))) 
-# =============================================================================
-coordinatesz.append(0)
-coordinatesy.append(constants.h /2. )
-coordinatesz.append(0)
-coordinatesy.append(- 1.* constants.h /2. )
+        coordinatesz[i + 1] = math.cos(alpha)* (l_straight - ((d_st * i - d_st/2) - (l_semi_circle + l_straight))) - (constants.C_a - constants.h/2)  #substract the (chord - radius semi-circle) of the aileron for z_coordinate with origin at hinge line
+        coordinatesy[i + 1] = math.sin(alpha)* (l_straight - ((d_st * i - d_st/2) - (l_semi_circle + l_straight)))
+        print (i, l_straight - (((d_st * i - d_st/2) - (l_semi_circle + l_straight))))
+
+coordinatesy[5] = constants.h /2.
+coordinatesy[9] = - 1.* constants.h /2. 
 
 
 plt.plot(coordinatesz, coordinatesy, 'ro')
-plt.axis([-.484 + 0.173/2, max(coordinates), -constants.h /2., max(coordinatesy) ])
+plt.axis([-.484, max(coordinatesz)+.02, -.2, .2])
 plt.show()
+
+
+#chord = constants.C_a
+#radius = constants.h/2.
+#n_st = number of stringers = constants.n_st
+def coordinate(chord, radius, n_st):
+    l_straight = math.sqrt((chord - radius)**2 + (radius)**2)                   #calculate length straight 
+    l_semi_circle = math.pi * radius                                            #aileron circumference of semi_circle
+    d_st = (2*l_straight + l_semi_circle )/(constants.n_st )                    #calculate length between 2 stringers
+    alpha = math.atan2(radius, chord - radius  )                                #calculate angle of straight line of aileron
+    coordinatesz = [0] * int(n_st +2)                                           #add 2 for rib booms
+    coordinatesy = [0] * int(n_st +2)
+       
+    for i in range(1, constants.n_st + 1):                                      #start at stringer number 1
+        if (d_st* i-  d_st/2.) < l_straight:
+            coordinatesz[i - 1] = math.cos(alpha)* (d_st * i -  d_st/2.) - (chord - radius)  #substract the (chord - radius semi-circle) of the aileron for z_coordinate with origin at hinge line
+            coordinatesy[i - 1] = - math.sin(alpha)* (d_st* i-  d_st/2.)
+            end_straight_length = d_st* i-  d_st/2.
+            j = copy.deepcopy(i)  
+        elif (d_st* i-  d_st/2.) < (l_semi_circle + l_straight):
+            beta = (d_st*i - d_st* j - (l_straight - end_straight_length)) / (radius) 
+            coordinatesz[i ] = math.sin((beta)) * radius
+            coordinatesy[i ] = - math.cos((beta)) * radius
+        elif (d_st* i-  d_st/2.) < (l_semi_circle + l_straight* 2.):
+            coordinatesz[i + 1] = math.cos(alpha)* (l_straight - ((d_st * i - d_st/2) - (l_semi_circle + l_straight))) - (chord - radius)  #substract the (chord - radius semi-circle) of the aileron for z_coordinate with origin at hinge line
+            coordinatesy[i + 1] = math.sin(alpha)* (l_straight - ((d_st * i - d_st/2) - (l_semi_circle + l_straight)))
+    coordinatesy[5] = radius
+    coordinatesy[9] = -1* radius
+
+    
