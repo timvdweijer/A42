214ecbd1e8cddcb41b761c18700da7120e40be70
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-22 18:46:41 +0100

    torsion update

--- Torsion.py
+++ Torsion.py
@@ -8,37 +8,38 @@ from math import *
 from constants import *
 import numpy as np
 import coordinates
+import matplotlib.pyplot as plt
 
 """
 Calculation of the torsion due to Fact, P and q around hinge 2 check if that's okay
 these are split up in the u,v,w components of the'new' reference frame
 """
-Fact = 37900 ################################################################import right value and sign
-theta_r = radians(theta)        #convert degrees to radians
+Fact = 42018.82258451103 ################################################################import right value and sign
+theta_r = radians(theta)                                    #convert degrees to radians
+q_w = sin(theta_r) * q                                      #z_component of q
+q_v = cos(theta_r) * q                                      #y_component of q
+P_w = cos(theta_r) * -1* P                                  #z_component of P
+P_v = sin(theta_r) * P                                      #y_component of P
+Fact_w = cos(theta_r) * -1* Fact                            #z_component of jammed actuator
+Fact_v = sin(theta_r) * Fact                                #y_component of jammed actuator
 
-q_w = sin(theta_r) * q
-q_v = cos(theta_r) * q 
-P_w = cos(theta_r) * -1* P
-P_v = sin(theta_r) * P
-Fact_w = cos(theta_r) * -1* Fact   ####change for right variable
-Fact_v = sin(theta_r) * Fact   ####change for right variable
-
-lstep = .001
-x = np.arange(0, l_a +lstep, lstep)
-
-T = [0]
-xx =[0]
+lstep = .001                                                #define spanwise step
+x = np.arange(0, l_a +lstep, lstep)                         #create array of x points spanwise
+T = []                                                      #create empty list for torque, the zero is to d
+xx =[]                                                      #create empty list for x_positions, the zero is to d
 for i in np.nditer(x):
     if i == 0:
-        T[0] = (-1*q_v*lstep* (0.25*C_a - h/2.))
-    elif i == .554:
-        Tq = (-1*q_v*lstep* (0.25*C_a - h/2.) )
-        TP = P_w * h/2 + -1* P_v *h/2
-        Tact = -1* (Fact_w * h/2 + -1* Fact_v *h/2)
-        T.append(Tq + TP +Tact + T[int(i*1000) - 1])
+        Tq = (-1*q_v*lstep* (0.25*C_a - h/2.))              #if x = 0 then calculate first torque due to q, loter on the value before is needed
+        T.append(Tq)                                        
+        xx.append(i)
+    elif i == .554:                                         #at hinge two where p and Fact also work
+        Tq = (-1*q_v*lstep* (0.25*C_a - h/2.))              #calculate torque due to q 
+        TP = P_w * h/2 + -1* P_v *h/2                       #calculate torque due to P
+        Tact = -1* (Fact_w * h/2 + -1* Fact_v *h/2)         #calculate torque due to 
+        T.append(Tq + TP +Tact + T[int(i*1000) - 1])        #append value to T_lst of all components + last component
         xx.append(i)        
     else:
-        Tq = (-1*q_v*lstep* (0.25*C_a - h/2.) )
+        Tq = (-1*q_v*lstep* (0.25*C_a - h/2.) )             #at all values except 0 and .554 add only q_acts
         T.append(Tq + T[int(i*1000) - 1])
         xx.append(i)
         
@@ -55,13 +56,15 @@ now you can solve #see page 613 of Megson aircraft structures
 matrix [qcell_1, qcell_2]
 s_.. = distance of element / thickness
 """
-T = 1 ###########################################################################################change this for for loop
-area_1 = pi * (h/2)**2 /2
+area_1 = pi * (h/2)**2 /2                                   # area's of cell 1 and two
 area_2 = (C_a- h/2) * h
-s_semi = pi* h/2 / t_sk
-s_rib = h / t_sp
-s_TE =  coordinates.coordinate(C_a, h/2, n_st)[2]*2 / t_sk
-
-A = np.array([[(s_semi + s_rib)/(2*area_1*G)+ (s_rib)/(2*area_2*G), -1*((s_TE + s_rib)/(2*area_2*G) + (s_rib)/(2*area_1*G))], [2*area_1, 2*area_2]])
-B = np.array([0, T])
-q1_T, q2_T = np.linalg.solve(A,B)
+s_semi = pi* h/2 / t_sk                                     #integral parts of formula of semi circle 
+s_rib = h / t_sp                                            #integral parts of formula of rib 
+s_TE =  coordinates.coordinate(C_a, h/2, n_st)[2]*2 / t_sk  #integral parts of formula of part with straight lines 
+q_T = []
+for i in range(0, len(T)):                                  #for all x as before calculate shear 1 and two
+    A = np.array([[(s_semi + s_rib)/(2*area_1*G)+ (s_rib)/(2*area_2*G), -1*((s_TE + s_rib)/(2*area_2*G) + (s_rib)/(2*area_1*G))], [2*area_1, 2*area_2]])
+    B = np.array([0, T[i]])
+    q1_T, q2_T = np.linalg.solve(A,B)
+    q_T.append([q1_T, q2_T])  
+    
