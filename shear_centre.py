7d2dbe337bf036cb302ddc212ed9537515798392
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-26 15:42:57 +0100

    kk

--- shear_centre.py
+++ shear_centre.py
@@ -10,34 +10,46 @@ import numpy as np
 
 def shearcentre(coordinatesy, boomarea, Izz):
     """
-cell II compute base shear flow
-""" 
-    d_q_ij_lst = []
+    cell II compute base shear flow
+    """ 
+    d_q_ij_lst_cellI = []
     for i in range(0,11 ): 
         if i <=5:
-            d_q_ij =   -1* boomarea / Izz * coordinatesy[i]
+            d_q_ij =   -1* boomarea / Izz * coordinatesy[i]           #add Sy          #as centroid of y_location is on y axis, it's just the coordinates
         else: 
             d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 3]
         
         if i == 0:                                                              #make list for the q's between all booms, symmetric which is supposed to be symmetric
-            d_q_ij_lst.append(d_q_ij)
+            d_q_ij_lst_cellI.append(d_q_ij)
         else:
-            d_q_ij_lst.append(d_q_ij + d_q_ij_lst[-1])
-    print (d_q_ij_lst)
+            d_q_ij_lst_cellI.append(d_q_ij + d_q_ij_lst[-1])
+
+    """
+    Cell I compute base shear flow
+    """
+    d_q_ij_lst_cellII = []
+    for i in range(0,5):
+        if i <=3:
+            d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 6]             #add Sy        #as centroid of y_location is on y axis, it's just the coordinates  
+            print (coordinatesy[i + 6])
+        else: 
+            d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 3]
+            print (coordinatesy[i + 1])
+        
+        if i == 0:                                                              #make list for the q's between all booms, symmetric which is supposed to be symmetric
+            d_q_ij_lst_cellII.append(d_q_ij)
+        else:
+            d_q_ij_lst_cellII.append(d_q_ij + d_q_ij_lst[-1])   
+
+
+shearcentre(coordinates.a[0], 1, 1)
+    
+    
 # =============================================================================
 #         if i <=5:
 #             d_q_ij =   boomarea[i] / Izz * coordinatesy[i]
 #         else: 
 #             d_q_ij =   boomarea[i + 4] / Izz * coordinatesy[i + 3]
 #         d_q_ij_lst.append(d_q_ij)
-# =============================================================================
-"""
-Cell I compute base shear flow
-"""
-        
-        
-shearcentre(coordinates.a[0], 1, 1)
-    
-    
-            
+# =============================================================================            
         
