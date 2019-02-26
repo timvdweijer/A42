d7aef3d578d016392a37b29efa45fed578bd60a2
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-26 17:12:57 +0100

    rrr

--- shear_centre.py
+++ shear_centre.py
@@ -6,50 +6,50 @@ Created on Mon Feb 25 22:36:28 2019
 """
 import coordinates
 import numpy as np
+import boom_area
 
 
-def shearcentre(coordinatesy, boomarea, Izz):
+def base_shear(coordinatesy, coordiantesz, Izz, Iyy, centroidy, centroidz, boomarea, Sy, Sz):
     """
     cell II compute base shear flow
     """ 
-    d_q_ij_lst_cellI = []
-    for i in range(0,11 ): 
-        if i <=5:
-            d_q_ij =   -1* boomarea / Izz * coordinatesy[i]           #add Sy          #as centroid of y_location is on y axis, it's just the coordinates
-        else: 
-            d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 3]
-        
-        if i == 0:                                                              #make list for the q's between all booms, symmetric which is supposed to be symmetric
-            d_q_ij_lst_cellI.append(d_q_ij)
-        else:
-            d_q_ij_lst_cellI.append(d_q_ij + d_q_ij_lst[-1])
+    q_cellII = []
+    for j in range(0, len(Sy)):
+        q_ij_lst_cellII = []
+        for i in range(0,11 ): 
+            if i <=5:
+                d_q_ij =   - Sy[j] * boomarea[j][i] / Izz[j] * (coordinatesy[i] - centroidy[j]) \          #vertical shearforce contribution
+                - Sz[j] * boomarea[j][i] / Iyy[j] * (coordinatesz[i] - centroidz[j])                        #horizotal shearforce contribution
+            else: 
+                d_q_ij =   - Sy[j] * boomarea[j][i + 3] / Izz[j] * (coordinatesy[i + 3] - centroidy[j]) \          #vertical shearforce contribution
+                - Sz[j] * boomarea[j][i + 3] / Iyy[j] * (coordinatesz[i + 3] - centroidz[j])                        #horizotal shearforce contribution
+            
+            if i == 0:                                                              #make list for the q's between all booms
+                q_ij_lst_cellII.append(d_q_ij)
+            else:
+                q_ij_lst_cellII.append(d_q_ij + q_ij_lst_cellII[-1])
 
-    """
-    Cell I compute base shear flow
-    """
-    d_q_ij_lst_cellII = []
-    for i in range(0,5):
-        if i <=3:
-            d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 6]             #add Sy        #as centroid of y_location is on y axis, it's just the coordinates  
-            print (coordinatesy[i + 6])
-        else: 
-            d_q_ij =   -1* boomarea / Izz * coordinatesy[i + 3]
-            print (coordinatesy[i + 1])
-        
-        if i == 0:                                                              #make list for the q's between all booms, symmetric which is supposed to be symmetric
-            d_q_ij_lst_cellII.append(d_q_ij)
-        else:
-            d_q_ij_lst_cellII.append(d_q_ij + d_q_ij_lst[-1])   
-
-
-shearcentre(coordinates.a[0], 1, 1)
     
-    
-# =============================================================================
-#         if i <=5:
-#             d_q_ij =   boomarea[i] / Izz * coordinatesy[i]
-#         else: 
-#             d_q_ij =   boomarea[i + 4] / Izz * coordinatesy[i + 3]
-#         d_q_ij_lst.append(d_q_ij)
-# =============================================================================            
+        """
+        Cell I compute base shear flow
+        """
+        q_ij_lst_cellI = []
+        for i in range(0,5):
+            if i <=3:
+                d_q_ij =   - Sy[j] * boomarea[j][i + 6] / Izz[j] * (coordinatesy[i + 6] - centroidy[j]) \          #vertical shearforce contribution
+                - Sz[j] * boomarea[j][i + 6] / Iyy[j] * (coordinatesz[i + 6] - centroidz[j])                        #horizotal shearforce contribution
+            else: 
+                d_q_ij =   - Sy[j] * boomarea[j][i + 3] / Izz[j] * (coordinatesy[i + 3] - centroidy[j]) \          #vertical shearforce contribution
+                - Sz[j] * boomarea[j][i + 3] / Iyy[j] * (coordinatesz[i + 3] - centroidz[j])
+            
+            if i == 0:                                                              #make list for the q's between all booms, symmetric which is supposed to be symmetric
+                q_ij_lst_cellI.append(d_q_ij)
+            else:
+                q_ij_lst_cellI.append(d_q_ij + q_ij_lst_cellI[-1])
         
+        q_cellI.append(q_ij_lst_cellI)
+        q_cellII.append(q_ij_lst_cellII)
+        
+    return (q_cellI, q_cellII )
+
+baseshear = base_shear(coordinates.a[0], coordinates.a[1], , , , , boom_area.boomareas, bendingsheardiagrams.Sy, bendingsheardiagrams.Sz)
