fa9e99d11d29a4b2aff3665ac2176d5b9e20a66e
peterattiliobos <peterattiliobos@gmail.com>
2019-02-26 17:37:14 +0100

    Update shear_centre.py

--- shear_centre.py
+++ shear_centre.py
@@ -38,6 +38,30 @@ Cell I compute base shear flow
         
 shearcentre(coordinates.a[0], 1, 1)
     
+
+#redundantshearflow:
+cellI=np.array([7,8,9,10,6])
+cellII=np.array([0,1,2,3,4,5,6,10,11,12,13,14])
+
+for i in range(3):
+    q_bIforce[i]=q_bI[i]*distance_lstI[i]
+    q_bIforcez[i]=q_bIforce[i]*np.abs((coordinatez(cellI(i+1))-coordinatez(cellI(i+1)))/distance_lstI(i))
+    q_bIforcey[i]=q_bIforce[i]*np.abs((coordinatey(cellI(i+1))-coordinatey(cellI(i+1)))/distance_lstI(i))
+    A11=q_bIforcez[i]*distance_midpointI_y[i]+q_bIforcey[i]*distance_midpointII_z[i]
+    A21=q_bIforce[i]
+for i in range(10):
+    q_bIIforce[i]=q_bII[i]*distance_lstII[i]
+    q_bIIforcez[i]=q_bIIforce[i]*np.abs((coordinatez(cellII(i+1))-coordinatez(cellII(i+1)))/distance_lstII(i))
+    q_bIIforcey[i]=q_bIIforce[i]*np.abs((coordinatey(cellII(i+1))-coordinatey(cellII(i+1)))/distance_lstII(i)) 
+    A12=q_bIIforcez[i]*distance_midpointII_y[i]+q_bIIforcey[i]*distance_midpointII_z[i]
+
     
+
+
+
+# =============================================================================
+# 
+# =============================================================================
+
             
         
