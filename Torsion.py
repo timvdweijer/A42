56ad9fcd996e38460ecb78e3614a95ab786548ea
timvdweijer <37894170+timvdweijer@users.noreply.github.com>
2019-02-27 21:53:14 +0100

    jj

--- Torsion.py
+++ Torsion.py
@@ -22,14 +22,14 @@ def heaviside(x):
         return 0
     elif x >= 0:
         return 1
-F_act = 39700
+F_act = 39.72774618*1000
 theta_r = radians(theta)                                    #convert degrees to radians
-q_w = sin(theta_r) * q                                      #z_component of q
+q_w = 0#sin(theta_r) * q                                      #z_component of q
 q_v = cos(theta_r) * q                                      #y_component of q
-P_w = cos(theta_r) * -1* P                                  #z_component of P
-P_v = sin(theta_r) * P                                      #y_component of P
-Fact_w = cos(theta_r) * -1* F_act                            #z_component of jammed actuator
-Fact_v = sin(theta_r) * F_act                                #y_component of jammed actuator
+P_w = cos(theta_r) * P                                  #z_component of P
+P_v = 0#sin(theta_r) * P                                      #y_component of P
+Fact_w = cos(theta_r) *  F_act                            #z_component of jammed actuator
+Fact_v = 0#sin(theta_r) * F_act                                #y_component of jammed actuator
 
 
 lstep = .001                                                #define spanwise step
@@ -60,10 +60,9 @@ Fact_w: no sign modification is needed because of direction ---> other sign for 
 # =============================================================================
 T = []
 xx = []
-for x in np.arange(0, l_a+lstep, lstep):
+for x in np.arange(0, l_a + lstep, lstep):
     T.append(-1*Fact_v * (h/2) * (heaviside(x-(x_2 - x_a / 2.))) + Fact_w * (h/2) * (heaviside(x-(x_2 - x_a / 2.))) + P_v * (h/2) * (heaviside(x-(x_2 + x_a / 2.))) - P_w * (h/2) * (heaviside(x-(x_2 + x_a / 2.))) +  q_v * x* (-1 * (0.25*C_a - h/2.)))
     xx.append(x*1000)
-    print (heaviside())
 plt.plot(xx ,T)
 # =============================================================================
 # T = []                                                      #create empty list for torque, the zero is to d
@@ -91,40 +90,40 @@ plt.plot(xx ,T)
 # plt.show()
 # =============================================================================
 
-"""
-find shear flow due to torsion; cell 1 is semicircular part, cell2 is TE
-deflection cell 1 = deflection cell 2 
-and T = Tcell_1 + Tcell_2 
-now you can solve #see page 613 of Megson aircraft structures
-matrix [qcell_1, qcell_2]
-s_.. = distance of element / thickness
-"""
 area_1 = pi * (h/2)**2 /2                                   #enclosed areas of cell 1 and two
 area_2 = (C_a- h/2) * h
 s_semi = pi* h/2 / t_sk                                     #integral parts of formula of semi circle 
 s_rib = h / t_sp                                            #integral parts of formula of rib 
 s_TE =  coordinates.coordinate(C_a, h/2, n_st)[2]*2 / t_sk  #integral parts of formula of part with straight lines 
-q_T = []
-for i in range(0, len(T)):                                  #for all x as before calculate shear 1 and two
-    A = np.array([[(s_semi + s_rib)/(2*area_1*G)+ (s_rib)/(2*area_2*G), -1*((s_TE + s_rib)/(2*area_2*G) + (s_rib)/(2*area_1*G))], [2*area_1, 2*area_2]])
-    B = np.array([0, T[i]])
-    q1_T, q2_T = np.linalg.solve(A,B)
-    q_T.append([q1_T, q2_T])  
 
+def shear_torque(T, s_semi, s_rib, s_TE, area_1, area_2):
+    """
+    find shear flow due to torsion; cell 1 is semicircular part, cell2 is TE
+    deflection cell 1 = deflection cell 2 
+    and T = Tcell_1 + Tcell_2 
+    now you can solve #see page 613 of Megson aircraft structures
+    matrix [qcell_1, qcell_2]
+    s_.. = distance of element / thickness
+    """
+    q_T = []
+    for i in range(0, len(T)):                                  #for all x as before calculate shear 1 and two
+        A = np.array([[(s_semi + s_rib)/(2*area_1*G)+ (s_rib)/(2*area_2*G), -1*((s_TE + s_rib)/(2*area_2*G) + (s_rib)/(2*area_1*G))], [2*area_1, 2*area_2]])
+        B = np.array([0, T[i]])
+        q1_T, q2_T = np.linalg.solve(A,B)
+        q_T.append([q1_T, q2_T]) 
+    return q_T
+q_T = shear_torque(T, s_semi, s_rib, s_TE, area_1, area_2)
 
-d_theta = (q_T[0][0]* (s_semi+s_rib) - q_T[0][1]*s_rib)/(2*area_1*G) * lstep
-def_theta = []                                              #deflection in terms of theta
-def_theta.append( (q_T[0][0]* (s_semi+s_rib) - q_T[0][1]*s_rib)/(2*area_1*G) * lstep) #p.613 formula megson where lstep = dz                                             #deflection theta
-for i in range(1, len(q_T[:])):
-    d_theta_torque = (q_T[i][0]* (s_semi+s_rib) - q_T[i][1]*s_rib)/(2*area_1*G) * lstep #p.613 formula megson where lstep = dz
-    def_theta.append(d_theta_torque + def_theta[i-1])
+def theta(q_T, lstep): #gives the angle at all location
+    theta = [] 
+    theta.append( (q_T[0][0]* (s_semi+s_rib) - q_T[0][1]*s_rib)/(2*area_1*G) * lstep) #p.613 formula megson where lstep = dz                                                   #deflection in terms of theta                                            #deflection theta
+    for i in range(1, len(q_T[:])):
+        d_theta_torque = (q_T[i][0]* (s_semi+s_rib) - q_T[i][1]*s_rib)/(2*area_1*G) * lstep #p.613 formula megson where lstep = dz
+        theta.append(d_theta_torque + theta[i-1])
+    return (theta)
 
+theta_corrected = theta(q_T, lstep) - theta(q_T, lstep)[int(x_1 *1000)] #assume deflection is zero at hinge 1 
 # =============================================================================
-# plt.plot(xx, q_T[:][0], xx, q_T[:][1]) ######################## beginning is at the end, not good
-# plt.show()
+# plt.plot(xx, theta_corrected)
 # =============================================================================
 
-# =============================================================================
-# plt.plot(xx, def_theta)
-# plt.show()
-# =============================================================================
