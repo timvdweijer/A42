3326008536b752dbe26eef8f1d3f73f98598f28d
SeraZalman <47747395+SeraZalman@users.noreply.github.com>
2019-03-01 12:03:04 +0100

    validation

--- deflectionreadingvalidation.py
+++ deflectionreadingvalidation.py
@@ -15,4 +15,20 @@ d_x = table[:,6]
 d_y = table[:,7]
 d_z = table [:,8]
 
+y_def = []
 
+defl= np.column_stack((node_n,d_mag,d_x,d_y,d_z))
+for i in range (len(defl)):
+    for j in range(len(le_number)):
+        if defl[i,0] == le_number[j]:
+            y_def.append(defl[i,3])
+            
+
+
+#ilist= []
+#
+#for i in range(len(defl)):
+#    for j in range(len(n_number)):
+#        if defl[i,0] == n_number[j]:
+#            ilist.append(defl[i,:])
+#            
